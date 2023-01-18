2b0746ca16ad0841f2f648bcfc3420a91e03df55
l-berot <110924413+l-berot@users.noreply.github.com>
2023-01-17 22:53:09 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("myButton");
+  button.addEventListener("click", myFunction);
+  
+  function myFunction() {
+    alert("Este CV fue creado como proyecto final para la formacion 'Primeros pasos del desarrollo front end' para Argentina Programa 4.0");
+  }
